37265219091bec3872a1c384b57460f62d4251a4
tunggulalmajid <tunggulmajid@gmail.com>
2025-05-24 20:50:54 +0700

    startpage

---
+++ project-ecoranger/Views/UcStartPage.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace project_ecoranger.Views
+{
+    partial class UcStartPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
+            registerbtn = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = Properties.Resources.tittleStartPage;
+            pictureBox2.Location = new Point(571, 287);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(878, 320);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = Properties.Resources.btnLoginStartPage;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.Location = new Point(772, 613);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(240, 100);
+            panel1.TabIndex = 2;
+            panel1.Click += panel1_Click;
+            panel1.Paint += panel1_Paint;
+            // 
+            // registerbtn
+            // 
+            registerbtn.BackColor = Color.Transparent;
+            registerbtn.BackgroundImage = Properties.Resources.btnRegisterStartPage;
+            registerbtn.BackgroundImageLayout = ImageLayout.Zoom;
+            registerbtn.Location = new Point(1018, 613);
+            registerbtn.Name = "registerbtn";
+            registerbtn.Size = new Size(240, 100);
+            registerbtn.TabIndex = 3;
+            registerbtn.Click += registerbtn_Click;
+            // 
+            // UcStartPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background;
+            Controls.Add(registerbtn);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "UcStartPage";
+            Size = new Size(1920, 1060);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Panel panel1;
+        private Panel registerbtn;
+    }
+}
